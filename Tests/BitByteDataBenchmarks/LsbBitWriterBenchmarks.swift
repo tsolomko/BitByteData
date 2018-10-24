7871466b04a92c07824128f14a3bdd9ffc89695e
Timofey Solomko <tsolomko@gmail.com>
2018-10-24 20:53:24 +0300

    Reduce number of write operations in bit writer benchmarks

--- Tests/BitByteDataBenchmarks/LsbBitWriterBenchmarks.swift
+++ Tests/BitByteDataBenchmarks/LsbBitWriterBenchmarks.swift
@@ -12,7 +12,7 @@ class LsbBitWriterBenchmarks: XCTestCase {
         self.measure {
             let bitWriter = LsbBitWriter()
 
-            for _ in 0..<5_000_000 * 4 {
+            for _ in 0..<4_000_000 {
                 bitWriter.write(bit: 0)
                 bitWriter.write(bit: 1)
             }
@@ -23,7 +23,7 @@ class LsbBitWriterBenchmarks: XCTestCase {
         self.measure {
             let bitWriter = LsbBitWriter()
 
-            for _ in 0..<5_000_000 {
+            for _ in 0..<1_000_000 {
                 bitWriter.write(number: 55, bitsCount: 7)
             }
         }
@@ -33,7 +33,7 @@ class LsbBitWriterBenchmarks: XCTestCase {
         self.measure {
             let bitWriter = LsbBitWriter()
 
-            for _ in 0..<5_000_000 {
+            for _ in 0..<1_000_000 {
                 bitWriter.append(byte: 37)
             }
         }
