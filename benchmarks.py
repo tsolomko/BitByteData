71e4c075201a997bd625dc90b9667512d6d6f7d0
Timofey Solomko <tsolomko@gmail.com>
2018-11-16 13:08:55 +0300

    Benchmarking tool now runs necessary commands itself instead of just processing output

--- benchmarks.py
+++ benchmarks.py
@@ -2,25 +2,35 @@
 
 import argparse
 import re
+import subprocess
 import sys
 
 def action_run(args):
     benchmarks = dict()
-
-    for line in sys.stdin:
-        line = line.rstrip()
-        print(line)
-        p = None
-        # Output format of 'swift test' differs between macOS and Linux platforms.
-        if sys.platform == "darwin":
-            p = re.compile(r"Test Case '-\[BitByteDataBenchmarks\.(.+Benchmarks) (test.+)\]'.+average: (\d+.\d+), "
-                            r"relative standard deviation: (\d+.\d+)\%")
-        elif sys.platform == "linux":
-            p = re.compile(r"Test Case '(.+Benchmarks)\.(test.+)'.+average: (\d+.\d+), "
-                            r"relative standard deviation: (\d+.\d+)\%")
-        else:
-            raise Exception("Unknown platform: " + sys.platform) 
         
+    # Output format of 'swift test' differs between macOS and Linux platforms.
+    regex = ""
+    if sys.platform == "darwin":
+        regex = (r"Test Case '-\[BitByteDataBenchmarks\.(.+Benchmarks) (test.+)\]'.+average: (\d+.\d+), "
+                r"relative standard deviation: (\d+.\d+)\%")
+    elif sys.platform == "linux":
+        regex = (r"Test Case '(.+Benchmarks)\.(test.+)'.+average: (\d+.\d+), "
+                r"relative standard deviation: (\d+.\d+)\%")
+    else:
+        raise Exception("Unknown platform: " + sys.platform) 
+    p = re.compile(regex)
+
+    args = ["swift", "test", "-c", "release", "--filter", "BitByteDataBenchmarks"]
+    # macOS version of 'swift test' outputs to stderr instead of stdout.
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        line = process.stdout.readline().decode()
+        if line == "" and process.poll() is not None:
+            break
+        sys.stdout.write(line)
+        sys.stdout.flush()
+        line = line.rstrip()
         matches = p.findall(line)
         if len(matches) == 1 and len(matches[0]) == 4:
             benchmark_name = matches[0][0]
@@ -31,6 +41,11 @@ def action_run(args):
             benchmark.append(test_name + " " + avg + " " + rel_std_dev + "%")
             benchmarks[benchmark_name] = benchmark
 
+    exit_code = process.returncode
+
+    if exit_code != 0:
+        raise subprocess.CalledProcessError(exit_code, args)
+    
     for benchmark_name, results in benchmarks.items():
         print(benchmark_name + ":")
         for result in results:
