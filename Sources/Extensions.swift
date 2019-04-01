9baa74a43801a7d3c9a9bfed9420ddd07d95599a
Timofey Solomko <tsolomko@gmail.com>
2019-04-01 23:00:20 +0300

    Mark Data extension functions as @inlinable

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -7,22 +7,22 @@ import Foundation
 
 extension Data {
 
-    @inline(__always)
+    @inlinable @inline(__always)
     func toU16() -> UInt16 {
         return self.withUnsafeBytes { $0.bindMemory(to: UInt16.self)[0] }
     }
 
-    @inline(__always)
+    @inlinable @inline(__always)
     func toU32() -> UInt32 {
         return self.withUnsafeBytes { $0.bindMemory(to: UInt32.self)[0] }
     }
 
-    @inline(__always)
+    @inlinable @inline(__always)
     func toU64() -> UInt64 {
         return self.withUnsafeBytes { $0.bindMemory(to: UInt64.self)[0] }
     }
 
-    @inline(__always)
+    @inlinable @inline(__always)
     func toByteArray(_ count: Int) -> [UInt8] {
         return self.withUnsafeBytes { $0.map { $0 } }
     }
