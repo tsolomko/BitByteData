6042236874d3eba924450231a61dc266b66a4eb3
Timofey Solomko <tsolomko@gmail.com>
2019-04-27 12:29:05 +0300

    Add ByteReader protocol

---
+++ Sources/ByteReader.swift
@@ -0,0 +1,36 @@
+// Copyright (c) 2019 Timofey Solomko
+// Licensed under MIT License
+//
+// See LICENSE for license information
+
+import Foundation
+
+public protocol ByteReader: AnyObject {
+
+    var size: Int { get }
+
+    var data: Data { get }
+
+    var offset: Int { get set }
+
+    var isFinished: Bool { get }
+
+    var bytesLeft: Int { get }
+
+    var bytesRead: Int { get }
+
+    init(data: Data)
+
+    func byte() -> UInt8
+
+    func bytes(count: Int) -> [UInt8]
+
+    func int(fromBytes count: Int) -> Int
+
+    func uint64(fromBytes count: Int) -> UInt64
+
+    func uint32(fromBytes count: Int) -> UInt32
+
+    func uint16(fromBytes count: Int) -> UInt16
+
+}
