d5d4f3c538bb78f4705c22d9db5b7c3a1a93f57f
Timofey Solomko <tsolomko@gmail.com>
2020-03-19 18:33:19 +0300

    Add code-paths to Extensions.swift to support Swift pre-5.0

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -9,22 +9,40 @@ extension Data {
 
     @inline(__always)
     func toU16() -> UInt16 {
-        return self.withUnsafeBytes { $0.bindMemory(to: UInt16.self)[0] }
+        #if compiler(>=5.0)
+            return self.withUnsafeBytes { $0.bindMemory(to: UInt16.self)[0] }
+        #else
+            return self.withUnsafeBytes { $0.pointee }
+        #endif
     }
 
     @inline(__always)
     func toU32() -> UInt32 {
-        return self.withUnsafeBytes { $0.bindMemory(to: UInt32.self)[0] }
+        #if compiler(>=5.0)
+            return self.withUnsafeBytes { $0.bindMemory(to: UInt32.self)[0] }
+        #else
+            return self.withUnsafeBytes { $0.pointee }
+        #endif
     }
 
     @inline(__always)
     func toU64() -> UInt64 {
-        return self.withUnsafeBytes { $0.bindMemory(to: UInt64.self)[0] }
+        #if compiler(>=5.0)
+            return self.withUnsafeBytes { $0.bindMemory(to: UInt64.self)[0] }
+        #else
+            return self.withUnsafeBytes { $0.pointee }
+        #endif
     }
 
     @inline(__always)
     func toByteArray(_ count: Int) -> [UInt8] {
-        return self.withUnsafeBytes { $0.map { $0 } }
+        #if compiler(>=5.0)
+            return self.withUnsafeBytes { $0.map { $0 } }
+        #else
+            return self.withUnsafeBytes {
+                [UInt8](UnsafeBufferPointer(start: $0, count: count))
+            }
+        #endif
     }
 
 }
