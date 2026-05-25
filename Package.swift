911ee139a8156d223741808d98a9f35dc746db1f
Timofey Solomko <tsolomko@gmail.com>
2026-05-25 16:31:07 +0800

    Increase minimum deployment targets on Apple platforms

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 let package = Package(
     name: "BitByteData",
     platforms: [
-        .macOS(.v14),
-        .iOS(.v17),
-        .tvOS(.v17),
-        .watchOS(.v10),
-        .visionOS(.v1)
+        .macOS(.v15),
+        .iOS(.v18),
+        .tvOS(.v18),
+        .watchOS(.v11),
+        .visionOS(.v2)
     ],
     products: [
         .library(
